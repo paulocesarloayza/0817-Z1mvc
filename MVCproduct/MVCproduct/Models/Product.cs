b5440957ebad72b6d34f81060f9bbc94e7fb02f8
visual_community@outlook.com <visual_community@outlook.com>
2019-08-17 10:07:04 -0400

    Solucion completa MVC

---
+++ MVCproduct/MVCproduct/Models/Product.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MVCproduct.Models
+{
+    public class Product
+    {
+        [Key]
+        public int productID { get; set; }
+
+        [Required]
+        [Display(Description ="Product Name")]
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+    }
+}
